5f57fb3569458401425f1d8cb7e58028bb06d9c9
Marcel Munevar <marcelmunevar@gmail.com>
2026-02-22 00:32:20 -0500

    re-added partial supply cost matches

--- app.js
+++ app.js
@@ -294,7 +294,6 @@ class BuildOrderHelper {
       }
 
       // Check for partial matches
-      /*
       for (const [unit, cost] of Object.entries(SUPPLY_COSTS)) {
         if (itemLower.includes(unit)) {
           let finalCost = cost;
@@ -307,7 +306,6 @@ class BuildOrderHelper {
           return;
         }
       }
-      */
 
       // Default to 0 if not found
       totalCost += 0;
